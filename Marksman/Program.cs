ab60382a06ebc90d659c9f0587cda76fc04adb77
xQx <xQxCPMxQx@gmail.com>
2015-07-28 00:03:20 +0300

    Added Game Ping Status

--- Marksman/Program.cs
+++ Marksman/Program.cs
@@ -302,6 +302,7 @@ namespace Marksman
 
         private static void Drawing_OnDraw(EventArgs args)
         {
+            Drawing.DrawText(Drawing.Width * 0.93f, Drawing.Height * 0.04f, Color.GreenYellow, "Ping: " + Game.Ping.ToString());
             var toD = CClass.Config.SubMenu("Drawings").Item("Draw.ToD").GetValue<bool>();
             if (toD)
             {
