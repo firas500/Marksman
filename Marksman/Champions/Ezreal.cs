08a01a47ba1d3bfc3b47bc1a6af521f531480863
xQx <xQxCPMxQx@gmail.com>
2015-08-02 21:58:57 +0300

    Update Ezreal.cs

--- Marksman/Champions/Ezreal.cs
+++ Marksman/Champions/Ezreal.cs
@@ -109,10 +109,11 @@ namespace Marksman.Champions
                     foreach (var minions in
                         vMinions.Where(
                             minions => minions.Health < ObjectManager.Player.GetSpellDamage(minions, SpellSlot.Q)))
-                        var qPredict = Q.GetPrediction(minions);
-                        var hithere = qPredict.CastPosition.Extend(ObjectManager.Player.Position, -140);
-                        if (qPredict.Hitchance >= HitChance.High)
-                            Q.Cast(hithere);
+                        
+                        var qP = Q.GetPrediction(minions);
+                        var hit = qP.CastPosition.Extend(ObjectManager.Player.Position, -140);
+                        if (qP.Hitchance >= HitChance.High)
+                            Q.Cast(hit);
                 }
             }
             
