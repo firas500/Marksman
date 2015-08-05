f2e860b03016403790da677f850781a2ead2df54
xQx <xQxCPMxQx@gmail.com>
2015-08-05 21:37:51 +0300

    Update Draven.cs

--- Marksman/Champions/Draven.cs
+++ Marksman/Champions/Draven.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using LeagueSharp;
@@ -22,6 +22,11 @@ namespace Marksman.Champions
         public static Spell Q, W, E, R;
         public int QStacks = 0;
 
+        private static string Tab
+        {
+            get { return "    "; }
+        }
+
         public Draven()
         {
             Q = new Spell(SpellSlot.Q);
@@ -30,10 +35,12 @@ namespace Marksman.Champions
             R = new Spell(SpellSlot.R, 20000);
             E.SetSkillshot(250f, 130f, 1400f, false, SkillshotType.SkillshotLine);
             R.SetSkillshot(400f, 160f, 2000f, false, SkillshotType.SkillshotLine);
+
             GameObject.OnCreate += OnCreateObject;
             GameObject.OnDelete += OnDeleteObject;
             AntiGapcloser.OnEnemyGapcloser += OnEnemyGapcloser;
             Interrupter2.OnInterruptableTarget += Interrupter2_OnInterruptableTarget;
+            Drawing.OnEndScene += DrawingOnOnEndScene;
             Utils.Utils.PrintMessage("Draven loaded.");
         }
 
@@ -55,39 +62,43 @@ namespace Marksman.Champions
 
         private static void OnDeleteObject(GameObject sender, EventArgs args)
         {
-            if (!(sender.Name.Contains("Q_reticle_self")))
+            if ((sender.Name.Contains("Q_reticle_self")))
             {
-                return;
-            }
-            for (var i = 0; i < ExistingReticles.Count; i++)
-            {
-                if (ExistingReticles[i].NetworkId == sender.NetworkId)
+                for (var i = 0; i < ExistingReticles.Count; i++)
                 {
-                    ExistingReticles.RemoveAt(i);
-                    return;
+                    if (ExistingReticles[i].NetworkId == sender.NetworkId)
+                    {
+                        ExistingReticles.RemoveAt(i);
+                        return;
+                    }
                 }
             }
-            //ExistingReticles.RemoveAll(reticle => reticle.NetworkId == sender.NetworkId);
-            //Packet.S2C.Ping.Encoded(new Packet.S2C.Ping.Struct(sender.Position.X, sender.Position.Y)).Process();
         }
 
         private static void OnCreateObject(GameObject sender, EventArgs args)
         {
-            if (!(sender.Name.Contains("Q_reticle_self")))
+            if ((sender.Name.Contains("Q_reticle_self")))
             {
-                return;
+                ExistingReticles.Add(
+                    new Reticles
+                    {
+                        Object = sender,
+                        NetworkId = sender.NetworkId,
+                        ReticlePos = sender.Position,
+                        ExpireTime = Game.Time + 1.20
+                    });
             }
-            //Console.WriteLine(sender.Name + sender.NetworkId + sender.Type);
-            //Packet.S2C.Ping.Encoded(new Packet.S2C.Ping.Struct(sender.Position.X, sender.Position.Y)).Process();
+        }
 
-            ExistingReticles.Add(
-                new Reticles
-                {
-                    Object = sender,
-                    NetworkId = sender.NetworkId,
-                    ReticlePos = sender.Position,
-                    ExpireTime = Game.Time + 1.20
-                });
+        private void DrawingOnOnEndScene(EventArgs args)
+        {
+            var rCircle = Config.Item("DrawRMini").GetValue<bool>();
+            if (rCircle)
+            {
+                var maxRRange = Config.Item("UseRCMaxR").GetValue<Slider>().Value;
+                var rMax = Config.Item("DrawRMax").GetValue<Circle>();
+                Utility.DrawCircle(ObjectManager.Player.Position, maxRRange, rMax.Color, 1, 23, true);
+            }
         }
 
         public override void Drawing_OnDraw(EventArgs args)
@@ -95,12 +106,12 @@ namespace Marksman.Champions
             var drawOrbwalk = Config.Item("DrawOrbwalk").GetValue<Circle>();
             var drawReticles = Config.Item("DrawReticles").GetValue<Circle>();
             var drawCatchRadius = Config.Item("DrawCatchRadius").GetValue<Circle>();
-            
+
             if (drawOrbwalk.Active)
             {
                 Render.Circle.DrawCircle(GetOrbwalkPos(), 100, drawOrbwalk.Color);
             }
-            
+
             if (drawReticles.Active)
             {
                 foreach (var existingReticle in ExistingReticles)
@@ -108,13 +119,14 @@ namespace Marksman.Champions
                     Render.Circle.DrawCircle(existingReticle.ReticlePos, 100, drawReticles.Color);
                 }
             }
-            
+
             if (drawCatchRadius.Active)
             {
                 if (GetOrbwalkPos() != Game.CursorPos &&
                     (ComboActive || LaneClearActive || Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LastHit))
                 {
-                    Render.Circle.DrawCircle(Game.CursorPos, Config.Item("CatchRadius").GetValue<Slider>().Value, Color.Red);
+                    Render.Circle.DrawCircle(Game.CursorPos, Config.Item("CatchRadius").GetValue<Slider>().Value,
+                        Color.Red);
                 }
                 else
                 {
@@ -128,10 +140,19 @@ namespace Marksman.Champions
             {
                 Render.Circle.DrawCircle(ObjectManager.Player.Position, E.Range, drawE.Color);
             }
-            var drawR = Config.Item("DrawR").GetValue<Circle>();
-            if (drawR.Active)
+
+            var drawRMin = Config.Item("DrawRMin").GetValue<Circle>();
+            if (drawRMin.Active)
             {
-                Render.Circle.DrawCircle(ObjectManager.Player.Position, 2000, drawR.Color);
+                var minRRange = Config.Item("UseRCMinR").GetValue<Slider>().Value;
+                Render.Circle.DrawCircle(ObjectManager.Player.Position, minRRange, drawRMin.Color, 2);
+            }
+
+            var drawRMax = Config.Item("DrawRMax").GetValue<Circle>();
+            if (drawRMax.Active)
+            {
+                var maxRRange = Config.Item("UseRCMaxR").GetValue<Slider>().Value;
+                Render.Circle.DrawCircle(ObjectManager.Player.Position, maxRRange, drawRMax.Color, 2);
             }
         }
 
@@ -148,60 +169,54 @@ namespace Marksman.Champions
             {
                 Orbwalker.SetOrbwalkingPoint(cursor);
             }
-
+            Obj_AI_Hero t;
             //Combo
-            var rtarget = TargetSelector.GetTarget(2000, TargetSelector.DamageType.Physical);
             if (ComboActive)
             {
-                var target = TargetSelector.GetTarget(550, TargetSelector.DamageType.Physical);
-                if (target == null)
+                var minRRange = Config.Item("UseRCMinR").GetValue<Slider>().Value;
+                var maxRRange = Config.Item("UseRCMaxR").GetValue<Slider>().Value;
+
+                t = TargetSelector.GetTarget(maxRRange, TargetSelector.DamageType.Physical);
+                if (!t.IsValidTarget())
                 {
                     return;
                 }
-                if (W.IsReady() && Config.Item("UseWC").GetValue<bool>() &&
+                if (W.IsReady() && Config.Item("UseWC").GetValue<bool>() && t.IsValidTarget(Orbwalking.GetRealAutoAttackRange(null) + 65) &&
                     ObjectManager.Player.Buffs.FirstOrDefault(
                         buff => buff.Name == "dravenfurybuff" || buff.Name == "DravenFury") == null)
                 {
                     W.Cast();
                 }
-                if (IsFleeing(target) && Config.Item("UseEC").GetValue<bool>())
+                if (IsFleeing(t) && Config.Item("UseEC").GetValue<bool>() && t.IsValidTarget(E.Range))
                 {
-                    E.Cast(target);
+                    E.Cast(t);
                 }
 
-                try
+                if (Config.Item("UseRC").GetValue<bool>() && R.IsReady())
                 {
-                    if (Config.Item("UseRC").GetValue<bool>() && R.GetHealthPrediction(target) <= 0)
+                    t = TargetSelector.GetTarget(maxRRange, TargetSelector.DamageType.Physical);
+                    if (t.Distance(ObjectManager.Player) >= minRRange && t.Distance(ObjectManager.Player) <= maxRRange &&
+                        t.Health < ObjectManager.Player.GetSpellDamage(t, SpellSlot.R) * 2)
+                    //R.GetHealthPrediction(target) <= 0)
                     {
-                        R.Cast(target);
+                        R.Cast(t);
                     }
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    throw;
-                }
             }
-            //Manual cast R
-            if (Config.Item("RManualCast").GetValue<KeyBind>().Active)
-            {
-                R.Cast(rtarget);
-            }
-
 
             //Peel from melees
             if (Config.Item("EPeel").GetValue<bool>()) //Taken from ziggs(by pq/esk0r)
             {
                 foreach (var pos in from enemy in ObjectManager.Get<Obj_AI_Hero>()
-                    where
-                        enemy.IsValidTarget() &&
-                        enemy.Distance(ObjectManager.Player) <=
-                        enemy.BoundingRadius + enemy.AttackRange + ObjectManager.Player.BoundingRadius &&
-                        enemy.IsMelee()
-                    let direction =
-                        (enemy.ServerPosition.To2D() - ObjectManager.Player.ServerPosition.To2D()).Normalized()
-                    let pos = ObjectManager.Player.ServerPosition.To2D()
-                    select pos + Math.Min(200, Math.Max(50, enemy.Distance(ObjectManager.Player)/2))*direction)
+                                    where
+                                        enemy.IsValidTarget() &&
+                                        enemy.Distance(ObjectManager.Player) <=
+                                        enemy.BoundingRadius + enemy.AttackRange + ObjectManager.Player.BoundingRadius &&
+                                        enemy.IsMelee()
+                                    let direction =
+                                        (enemy.ServerPosition.To2D() - ObjectManager.Player.ServerPosition.To2D()).Normalized()
+                                    let pos = ObjectManager.Player.ServerPosition.To2D()
+                                    select pos + Math.Min(200, Math.Max(50, enemy.Distance(ObjectManager.Player) / 2)) * direction)
                 {
                     E.Cast(pos.To3D());
                 }
@@ -233,6 +248,12 @@ namespace Marksman.Champions
             config.AddItem(new MenuItem("UseWC", "Use W").SetValue(true));
             config.AddItem(new MenuItem("UseEC", "Use E").SetValue(true));
             config.AddItem(new MenuItem("UseRC", "Use R").SetValue(true));
+            config.AddItem(new MenuItem("UseRCMinR", Tab + "Min. R Range").SetValue(new Slider(350, 200, 750)));
+            config.AddItem(new MenuItem("UseRCMaxR", Tab + "Max. R Range").SetValue(new Slider(1000, 750, 3000)));
+            config.AddItem(new MenuItem("DrawRMin", Tab + "Draw Min. R Range").SetValue(new Circle(true, Color.DarkRed)));
+            config.AddItem(new MenuItem("DrawRMax", Tab + "Draw Max. R Range").SetValue(new Circle(true, Color.DarkMagenta)));
+            config.AddItem(new MenuItem("DrawRMini", Tab + "Draw R on Mini Map").SetValue(true));
+
             return true;
         }
 
@@ -247,9 +268,6 @@ namespace Marksman.Champions
             config.AddItem(
                 new MenuItem("DrawE", "E range").SetValue(new Circle(true, Color.FromArgb(100, 255, 0, 255))));
             config.AddItem(
-                new MenuItem("DrawR", "R range(2000 units)").SetValue(
-                    new Circle(true, Color.FromArgb(100, 255, 0, 255))));
-            config.AddItem(
                 new MenuItem("DrawOrbwalk", "Draw orbwalk position").SetValue(new Circle(true, Color.Yellow)));
             config.AddItem(new MenuItem("DrawReticles", "Draw on reticles").SetValue(new Circle(true, Color.Green)));
             config.AddItem(new MenuItem("DrawCatchRadius", "Draw Catch Radius").SetValue(new Circle(true, Color.Green)));
@@ -261,8 +279,7 @@ namespace Marksman.Champions
             config.AddItem(new MenuItem("maxqamount", "Max Qs to use simultaneous").SetValue(new Slider(2, 4, 1)));
             config.AddItem(new MenuItem("EGapCloser", "Auto E Gap closers").SetValue(true));
             config.AddItem(new MenuItem("EInterruptable", "Auto E interruptable spells").SetValue(true));
-            config.AddItem(new MenuItem("RManualCast", "Cast R Manually(2000 range)"))
-                .SetValue(new KeyBind("T".ToCharArray()[0], KeyBindType.Press));
+            //config.AddItem(new MenuItem("RManualCast", "Cast R Manually(2000 range)")).SetValue(new KeyBind("T".ToCharArray()[0], KeyBindType.Press));
             config.AddItem(new MenuItem("Epeel", "Peel self with E").SetValue(true));
             config.AddItem(new MenuItem("CatchRadius", "Axe catch radius").SetValue(new Slider(600, 200, 1000)));
             return true;
@@ -289,7 +306,7 @@ namespace Marksman.Champions
                         reticle =>
                             reticle.ReticlePos.Distance(cursor) <= Config.Item("CatchRadius").GetValue<Slider>().Value &&
                             reticle.Object.IsValid &&
-                            myHero.GetPath(reticle.ReticlePos).ToList().To2D().PathLength()/myHero.MoveSpeed + Game.Time <
+                            myHero.GetPath(reticle.ReticlePos).ToList().To2D().PathLength() / myHero.MoveSpeed + Game.Time <
                             reticle.ExpireTime);
 
             return reticles != null && myHero.Distance(reticles.ReticlePos) >= 100 ? reticles.ReticlePos : cursor;
