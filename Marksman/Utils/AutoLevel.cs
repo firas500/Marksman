c6fc810740939bef89f030054959f2dece4610d0
xQx <xQxCPMxQx@gmail.com>
2015-10-17 20:08:08 +0300

    updated kalista spell level

--- Marksman/Utils/AutoLevel.cs
+++ Marksman/Utils/AutoLevel.cs
@@ -72,7 +72,7 @@ namespace Marksman.Utils
                     break;
 
                 case "kalista":
-                    SpellLevels = new int[] { 2, 1, 3, 3, 3, 4, 1, 3, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2 };
+                    SpellLevels = new int[] { 2, 3, 1, 3, 3, 4, 3, 1, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2 };
                     //LocalMenu.AddItem(new MenuItem("AutoLevel." + championName, GetLevelList(SpellLevels)));
                     break;
 
