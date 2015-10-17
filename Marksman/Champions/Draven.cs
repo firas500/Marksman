607ed55de89f084dbb5b4cff580669a7268a6dfc
xQx <xQxCPMxQx@gmail.com>
2015-10-17 10:44:39 +0300

    Update Draven.cs

--- Marksman/Champions/Draven.cs
+++ Marksman/Champions/Draven.cs
@@ -326,7 +326,7 @@ namespace Marksman.Champions
         }
         public override bool JungleClearMenu(Menu config)
         {
-            return false;
+            return true;
         }
     }
 }
