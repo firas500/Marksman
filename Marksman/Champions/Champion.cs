44d6059cbcdbec24a62da41f394caaeaa7736763
xQx <xQxCPMxQx@gmail.com>
2015-10-28 23:20:15 +0200

    Update Champion.cs

--- Marksman/Champions/Champion.cs
+++ Marksman/Champions/Champion.cs
@@ -59,25 +59,14 @@ namespace Marksman.Champions
             return false;
         }
 
-        public virtual void Drawing_OnDraw(EventArgs args)
-        {
-        }
-
-        public virtual void Game_OnGameUpdate(EventArgs args)
-        {
-        }
-
-        public virtual void Orbwalking_AfterAttack(AttackableUnit unit, AttackableUnit target)
-        {
-        }
-
-        public virtual void Orbwalking_BeforeAttack(Orbwalking.BeforeAttackEventArgs args)
-        {
-        }
-
-        public virtual void Obj_AI_Base_OnProcessSpellCast(
-            Obj_AI_Base sender,
-            GameObjectProcessSpellCastEventArgs args)
-        { }
+        public virtual void Drawing_OnDraw(EventArgs args) { }
+        public virtual void Game_OnGameUpdate(EventArgs args) { }
+        public virtual void Orbwalking_AfterAttack(AttackableUnit unit, AttackableUnit target) { }
+        public virtual void Orbwalking_BeforeAttack(Orbwalking.BeforeAttackEventArgs args) { }
+        public virtual void OnCreateObject(GameObject sender, EventArgs args) { }
+        public virtual void OnDeleteObject(GameObject sender, EventArgs args) { }
+        public virtual void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args) { }
+        public virtual void Obj_AI_Base_OnBuffAdd(Obj_AI_Base sender, Obj_AI_BaseBuffAddEventArgs args) { }
+        public virtual void Obj_AI_Base_OnBuffRemove(Obj_AI_Base sender, Obj_AI_BaseBuffRemoveEventArgs args) { }
     }
 }
