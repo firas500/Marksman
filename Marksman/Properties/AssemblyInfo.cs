6b9f433b643f649b19040967310805eff09911f8
xQx <xQxCPMxQx@gmail.com>
2015-10-28 23:21:09 +0200

    Update AssemblyInfo.cs

--- Marksman/Properties/AssemblyInfo.cs
+++ Marksman/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("5.20.0.4")]
-[assembly: AssemblyFileVersion("5.20.0.4")]
+[assembly: AssemblyVersion("5.20.0.5")]
+[assembly: AssemblyFileVersion("5.20.0.5")]
