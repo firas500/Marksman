34f9eab74df3f67c957ff60dcf6b727cf9d7e4a8
xQx <xQxCPMxQx@gmail.com>
2015-10-28 19:49:44 +0200

    Update AutoLevel.cs

--- Marksman/Utils/AutoLevel.cs
+++ Marksman/Utils/AutoLevel.cs
@@ -77,7 +77,7 @@ namespace Marksman.Utils
                     break;
 
                 case "kindred":
-                    SpellLevels = new int[] { 1, 3, 2, 1, 1, 4, 3, 3, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2 };
+                    SpellLevels = new int[] { 2, 1, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3 , 3 };
                     //LocalMenu.AddItem(new MenuItem("AutoLevel." + championName, GetLevelList(SpellLevels)));
                     break;
 
