c9137702bbf7f9130012e8b49a1a77186f852e20
xQx <xQxCPMxQx@gmail.com>
2015-10-28 12:04:22 +0200

    Update MissFortune.cs

--- Marksman/Champions/MissFortune.cs
+++ Marksman/Champions/MissFortune.cs
@@ -13,6 +13,7 @@ namespace Marksman.Champions
     {
         public static Spell Q, W, E;
         private static float UltiCastedTime = 0;
+        public static Obj_AI_Hero Player = ObjectManager.Player;
 
         public MissFortune()
         {
@@ -24,6 +25,7 @@ namespace Marksman.Champions
             E = new Spell(SpellSlot.E, 800);
             E.SetSkillshot(0.5f, 100f, float.MaxValue, false, SkillshotType.SkillshotCircle);
             Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
+            Game.OnWndProc += Game_OnWndProc;
 
             Utils.Utils.PrintMessage("MissFortune loaded.");
         }
@@ -77,6 +79,16 @@ namespace Marksman.Champions
 
         public override void Game_OnGameUpdate(EventArgs args)
         {
+            if (this.JungleClearActive)
+            {
+                ExecuteJungleClear();
+            }
+
+            if (this.LaneClearActive)
+            {
+                ExecuteLaneClear();
+            }
+            return;
             var ultCasting = Game.Time - UltiCastedTime < 0.2 || ObjectManager.Player.IsChannelingImportantSpell();
             Orbwalking.Attack = !ultCasting;
             Orbwalking.Move = !ultCasting;
@@ -137,6 +149,185 @@ namespace Marksman.Champions
             }
         }
 
+        private void ExecuteJungleClear()
+        {
+            var jungleMobs = Marksman.Utils.Utils.GetMobs(Q.Range, Marksman.Utils.Utils.MobTypes.All);
+
+            if (jungleMobs != null)
+            {
+                if (Q.IsReady())
+                {
+                    switch (Program.Config.Item("Jungle.UseQ").GetValue<StringList>().SelectedIndex)
+                    {
+                        case 1:
+                        {
+                            Q.CastOnUnit(jungleMobs);
+                            break;
+                        }
+                        case 2:
+                        {
+                            jungleMobs = Utils.Utils.GetMobs(Q.Range, Utils.Utils.MobTypes.BigBoys);
+                            if (jungleMobs != null)
+                            {
+                                Q.CastOnUnit(jungleMobs);
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                if (W.IsReady())
+                {
+                    var jW = Program.Config.Item("Jungle.UseW").GetValue<StringList>().SelectedIndex;
+                    if (jW != 0)
+                    {
+                        if (jW == 1)
+                        {
+                            jungleMobs = Utils.Utils.GetMobs(Orbwalking.GetRealAutoAttackRange(null) + 65,
+                                Utils.Utils.MobTypes.BigBoys);
+                            if (jungleMobs != null)
+                            {
+                                W.Cast();
+                            }
+                        }
+                        else
+                        {
+                            var totalAa =
+                                ObjectManager.Get<Obj_AI_Minion>()
+                                    .Where(
+                                        m =>
+                                            m.Team == GameObjectTeam.Neutral &&
+                                            m.IsValidTarget(Orbwalking.GetRealAutoAttackRange(null) + 165))
+                                    .Sum(mob => (int) mob.Health);
+
+                            totalAa = (int) (totalAa/ObjectManager.Player.TotalAttackDamage());
+                            if (totalAa >= jW)
+                            {
+                                W.Cast();
+                            }
+
+                        }
+                    }
+                }
+
+                if (E.IsReady())
+                {
+                    var jE = Program.Config.Item("Jungle.UseE").GetValue<StringList>().SelectedIndex;
+                    if (jE != 0)
+                    {
+                        var aMobs = MinionManager.GetMinions(ObjectManager.Player.Position, E.Range, MinionTypes.All,
+                            MinionTeam.Neutral);
+                        if (aMobs.Count > jE)
+                        {
+                            E.Cast(aMobs[0]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Game_OnWndProc(WndEventArgs args)
+        {
+            if (args.Msg != 0x20a)
+                return;
+
+            Program.Config.Item("Lane.Enabled").SetValue(!Program.Config.Item("Lane.Enabled").GetValue<bool>());
+        }
+
+
+        public override void Orbwalking_BeforeAttack(Orbwalking.BeforeAttackEventArgs args)
+        {
+            if (!W.IsReady())
+            {
+                return;
+            }
+
+            if (GetValue<bool>("Misc.UseW.Turret") && args.Target is Obj_AI_Turret)
+            {
+                if (((Obj_AI_Turret) args.Target).Health >= Player.TotalAttackDamage*3)
+                {
+                    W.Cast();
+                }
+            }
+
+            if (GetValue<bool>("Misc.UseW.Inhibitor") && args.Target is Obj_BarracksDampener)
+            {
+                if (((Obj_BarracksDampener) args.Target).Health >= Player.TotalAttackDamage*3)
+                {
+                    W.Cast();
+                }
+            }
+
+            if (GetValue<bool>("Misc.UseW.Nexus") && args.Target is Obj_HQ)
+            {
+                W.Cast();
+            }
+        }
+
+        private void ExecuteLaneClear()
+        {
+            if (Q.IsReady())
+            {
+                var lQ = Program.Config.Item("Lane.UseQ").GetValue<StringList>().SelectedIndex;
+                if (lQ != 0)
+                {
+                    {
+                        var vMinions = MinionManager.GetMinions(ObjectManager.Player.Position, Q.Range);
+                        foreach (var minions in
+                            vMinions.Where(
+                                minions => minions.Health < ObjectManager.Player.GetSpellDamage(minions, SpellSlot.Q)))
+                        {
+                            Q.CastOnUnit(minions);
+                        }
+                    }
+                }
+            }
+
+            if (W.IsReady())
+            {
+                var lW = Program.Config.Item("Lane.UseW").GetValue<StringList>().SelectedIndex;
+                if (lW != 0)
+                {
+                    var totalAa =
+                        ObjectManager.Get<Obj_AI_Minion>()
+                            .Where(
+                                m =>
+                                    m.IsEnemy && !m.IsDead &&
+                                    m.IsValidTarget(Orbwalking.GetRealAutoAttackRange(null)))
+                            .Sum(mob => (int) mob.Health);
+
+                    totalAa = (int) (totalAa/ObjectManager.Player.TotalAttackDamage());
+                    if (totalAa > lW)
+                    {
+                        W.Cast();
+                    }
+                }
+            }
+
+            if (E.IsReady())
+            {
+                var lE = Program.Config.Item("Lane.UseE").GetValue<StringList>().SelectedIndex;
+                if (lE != 0)
+                {
+                    var mE = MinionManager.GetMinions(ObjectManager.Player.ServerPosition, E.Range, MinionTypes.All);
+                    if (mE != null)
+                    {
+                        var locE = E.GetCircularFarmLocation(mE, 175);
+                        if (locE.MinionsHit >= lE && E.IsInRange(locE.Position.To3D()))
+                        {
+                            foreach (
+                                var x in
+                                    ObjectManager.Get<Obj_AI_Minion>()
+                                        .Where(m => m.IsEnemy && !m.IsDead && m.Distance(locE.Position) < 500))
+                            {
+                                E.Cast(x);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         public override bool ComboMenu(Menu config)
         {
             config.AddItem(new MenuItem("UseQC" + Id, "Use Q").SetValue(true));
@@ -153,10 +344,10 @@ namespace Marksman.Champions
             config.AddItem(new MenuItem("UseEH" + Id, "Use E").SetValue(true));
             config.AddItem(
                 new MenuItem("UseQTH" + Id, "Use Q (Toggle)").SetValue(new KeyBind("H".ToCharArray()[0],
-                    KeyBindType.Toggle)));
+                    KeyBindType.Toggle))).Permashow(true, "Marksman | Toggle Q", SharpDX.Color.Aqua);
             config.AddItem(
                 new MenuItem("UseETH" + Id, "Use E (Toggle)").SetValue(new KeyBind("T".ToCharArray()[0],
-                    KeyBindType.Toggle)));
+                    KeyBindType.Toggle))).Permashow(true, "Marksman | Toggle E", SharpDX.Color.Aqua);
 
             return true;
         }
@@ -164,24 +355,81 @@ namespace Marksman.Champions
         public override bool DrawingMenu(Menu config)
         {
             config.AddItem(
-                new MenuItem("DrawQ" + Id, "Q range").SetValue(
-                    new Circle(true, Color.FromArgb(100, 255, 0, 255))));
-
+                new MenuItem("DrawQ" + Id, "Q range").SetValue(new Circle(true, Color.FromArgb(50, 255, 0, 255))));
             config.AddItem(
-                new MenuItem("DrawE" + Id, "E range").SetValue(
-                    new Circle(false, Color.FromArgb(100, 255, 255, 255))));
+                new MenuItem("DrawE" + Id, "E range").SetValue(new Circle(false, Color.FromArgb(50, 255, 255, 255))));
+            config.AddItem(
+                new MenuItem("DrawE" + Id, "R range").SetValue(new Circle(false, Color.FromArgb(100, 255, 255, 255))));
 
             return true;
         }
 
         public override bool LaneClearMenu(Menu config)
         {
-            config.AddItem(new MenuItem("UseQL" + Id, "Use Q").SetValue(true));
+            config.AddItem(new MenuItem("Lane.Enabled", "Enable! (On/Off: Mouse Scroll").SetValue(true))
+                .Permashow(true, "Marksman | Enable Farm", SharpDX.Color.Aqua);
+
+            config.AddItem(
+                new MenuItem("Lane.UseQ", Utils.Utils.Tab + "Use Q:").SetValue(new StringList(new[] {"Off", "On"})));
+
+            string[] strW = new string[7];
+            {
+                strW[0] = "Off";
+
+                for (var i = 1; i < 7; i++)
+                {
+                    strW[i] = "If need to AA more than >= " + i;
+                }
+                config.AddItem(new MenuItem("Lane.UseW", Utils.Utils.Tab + "Use W:").SetValue(new StringList(strW, 0)));
+            }
+
+            string[] strE = new string[5];
+            {
+                strE[0] = "Off";
+
+                for (var i = 1; i < 5; i++)
+                {
+                    strE[i] = "Minion Count >= " + i;
+                }
+                config.AddItem(new MenuItem("Lane.UseE", Utils.Utils.Tab + "Use E:").SetValue(new StringList(strE, 0)));
+            }
             return true;
         }
+
         public override bool JungleClearMenu(Menu config)
         {
-            return false;
+            config.AddItem(
+                new MenuItem("Jungle.UseQ", "Use Q").SetValue(new StringList(new[] {"Off", "On", "Just big Monsters"}, 2)));
+
+            string[] strW = new string[8];
+            strW[0] = "Off";
+            strW[1] = "Just for big Monsters";
+
+            for (var i = 2; i < 8; i++)
+            {
+                strW[i] = "If need to AA more than >= " + i;
+            }
+
+            config.AddItem(new MenuItem("Jungle.UseW", "Use W").SetValue(new StringList(strW, 4)));
+
+            string[] strE = new string[4];
+            strE[0] = "Off";
+
+            for (var i = 1; i < 4; i++)
+            {
+                strE[i] = "Mob Count >= " + i;
+            }
+
+            config.AddItem(new MenuItem("Jungle.UseE", "Use E:").SetValue(new StringList(strE, 3)));
+            return true;
+        }
+
+        public override bool MiscMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("Misc.UseW.Turret" + Id, "Use W for Turret").SetValue(true));
+            config.AddItem(new MenuItem("Misc.UseW.Inhibitor" + Id, "Use W for Inhibitor").SetValue(true));
+            config.AddItem(new MenuItem("Misc.UseW.Nexus" + Id, "Use W for Nexus").SetValue(true));
+            return true;
         }
     }
 }
