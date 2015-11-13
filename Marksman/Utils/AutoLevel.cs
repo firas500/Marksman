6b96e6f0984a98d4f794195c289066e0584b0530
xQx <xQxCPMxQx@gmail.com>
2015-11-13 17:24:53 +0200

    Updated Caitlyn AutoLevel

--- Marksman/Utils/AutoLevel.cs
+++ Marksman/Utils/AutoLevel.cs
@@ -32,7 +32,7 @@ namespace Marksman.Utils
                     break;
 
                 case "caitlyn":
-                    SpellLevels = new int[] { 1, 2, 3, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
+                    SpellLevels = new int[] { 1, 3, 2, 1, 1, 4, 3, 1, 2, 1, 4, 3, 2, 3, 2, 4, 3, 2 };
                     //LocalMenu.AddItem(new MenuItem("AutoLevel." + championName, GetLevelList(SpellLevels)));
                     break;
 
